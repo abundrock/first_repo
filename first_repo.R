d92deee071e051929e46c483ab4bcb6f3cbbdb1a
abundrock <abundrock7@gmail.com>
2026-09-22 13:21:52 -0600

    retest

--- first_repo.R
+++ first_repo.R
@@ -3,6 +3,6 @@ install.packages("usethis")
 library(usethis)
 
 usethis::use_git_config(
-  user.name = "Your Name", 
-  user.email = "your.email@example.com"
+  user.name = "abundrock", 
+  user.email = "abundrock7@gmail.com"
 )
